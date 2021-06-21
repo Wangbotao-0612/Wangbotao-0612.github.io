3a42a2dedadfc0b535ee9db15a7b3feb8cde52ea
Wangbotao-0612 <59598800+Wangbotao-0612@users.noreply.github.com>
2021-06-21 23:49:04 +0800

    Create Demo02.java
    我的作品

---
+++ Demo02.java
@@ -0,0 +1,24 @@
+public class Demo02 {
+    public static void main(String[] args) {
+        //八大基本数据类型
+
+        //整数
+        int num1 = 10;
+        byte num2 = 20;
+        short num3 = 30;
+        long num4 = 30L;// Long类型后面加L
+
+        //小数：浮点数
+        float num5 = 50.1F;//float类型后面加F
+        double num6 = 3.141592653589793;
+
+        //字符
+        char name = 'A';
+        //字符串,Strin不是关键字，类
+        String namea = "秦疆";
+
+        //布尔值:是非
+        boolean flag = true;
+        //boolean flag = false;
+    }
+}
